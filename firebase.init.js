63b9ce73c5f9c157675c0eb0238d792990fcb6a1
sumon232 <sumonmia4526@gmail.com>
2024-12-29 00:52:16 +0600

    Alhamdulillah firebase init and basic folder structure making done

---
+++ firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqQBd_OgnMqt81uOo7HyI9A9kVIHGD3bk",
+  authDomain: "ph-assignment-11-e59b9.firebaseapp.com",
+  projectId: "ph-assignment-11-e59b9",
+  storageBucket: "ph-assignment-11-e59b9.firebasestorage.app",
+  messagingSenderId: "583972969203",
+  appId: "1:583972969203:web:43766e54fcd02744557586",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
